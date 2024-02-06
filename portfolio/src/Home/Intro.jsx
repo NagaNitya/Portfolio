de73f8fd1c892e9af9a93f1270352aa9d0aaf04d
NagaNitya <naganitya@gmail.com>
2024-02-06 08:17:44 +0530

    change to intro, Test netlify continuous deployment

--- portfolio/src/Home/Intro.jsx
+++ portfolio/src/Home/Intro.jsx
@@ -5,7 +5,7 @@ const Intro = () => {
         <div>
             <div className='intro_div'>
                 <h2>Hello everyone, and welcome to my personal portfolio!</h2><br /><br />
-                <p>My name is Nitya Penumerthy, I am currently pursuing my Bachelor of Technology, majoring in Computer Science. I enjoy coding and creating things with code, which has led to my creating this portfolio to showcase all the projects I have done.</p>
+                <p>My name is Nitya Penumerthy, I am currently pursuing my Bachelor of Technology, majoring in Computer Science. I enjoy coding and creating things with code, which has led to my creating this portfolio to showcase all the projects I have done, and am currently working on.</p>
             </div>
             <p className='more_info_p'>Click on the links to see my projects, my skills and my credentials in other websites</p>
         </div>
