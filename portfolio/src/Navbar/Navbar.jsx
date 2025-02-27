66935a021ec599bcacc20a25002659c9dfd12321
NagaNitya <naganitya@gmail.com>
2025-02-27 19:50:09 +0530

    added animation to navbar

--- portfolio/src/Navbar/Navbar.jsx
+++ portfolio/src/Navbar/Navbar.jsx
@@ -5,8 +5,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div className='main_nav'>
-            <Link to={'/home'} className='nav_link_head'><h2>Nitya Penumerthy</h2>
-            </Link>           
+            <Link to={'/home'} className='nav_link_head'><h2>Nitya Penumerthy</h2></Link>           
             <Link to={'/projects'} className='nav_link'>Projects</Link>
             <Link to={'/about'} className='nav_link'>About</Link>
             <Link to={'/gallery'} className='nav_link'>Gallery</Link>
